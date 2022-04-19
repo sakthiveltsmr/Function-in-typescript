26568b3815adf340ae3cdbc83e3b456c7db8357e
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-04-19 12:37:28 +0530

    first commit

---
+++ Function.js
@@ -0,0 +1,5 @@
+//function declaration
+function main(num1, num2) {
+    return console.log(num1 * num2);
+}
+main(2, 3);
---
+++ Function.ts
@@ -0,0 +1,6 @@
+//function declaration
+function main(num1: number, num2: number) {
+  return console.log(num1 * num2);
+}
+
+main(2, 3);
