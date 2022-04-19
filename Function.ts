19a323d6b8bac9e5b4ceeb09965a2adbdcd82345
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-04-19 12:45:14 +0530

    assign value

--- Function.ts
+++ Function.ts
@@ -5,3 +5,13 @@ function main(num1: number, num2: number) {
 }
 
 main(2, 3);
+
+//function with parameter assign value
+
+function main2(num1: number, num2: number = 5) {
+  return console.log(num1 + num2);
+}
+
+main2(3);
+
+//function
